6027889abcdf9a0674fe690192f6962cdc02f035
Uniminutoarduino <digpot@gmail.com>
2022-04-24 16:53:26 -0500

    Update leerarea.ino

--- PROCOLO-S7/SETTIMINO/leerarea.ino
+++ PROCOLO-S7/SETTIMINO/leerarea.ino
@@ -2,7 +2,7 @@
 Código modificado tomado de ReadDemo proporcionado por los desarrolladores de Settimino
 Código creado: 23 de abril/2022
 Autor: taloselectrónico
-//Si se comentarea la línea DO_IT SMALL iincluir este buffer ára la transmisión de datos
+//Si se comentarea la línea DO_IT SMALL incluir este buffer para la transmisión de datos
 
   Size=1024;
   Target = &Buffer; // Uses a larger buffer
@@ -20,45 +20,22 @@ byte mac[] = {
 
 IPAddress Local(192,168,0,90); // IP de Arduino+W5100
 IPAddress PLC(192,168,0,1);   // Dirección IP PLC
-
-// Following constants are needed if you are connecting via WIFI
-// The ssid is the name of my WIFI network (the password obviously is wrong)
-//char ssid[] = "SKYNET-AIR";    // Your network SSID (name)
-//char pass[] = "yourpassword";  // Your network password (if any)
 IPAddress Gateway(192, 168, 0, 3); //Dirección IP Gateway (Router)
 IPAddress Subnet(255, 255, 255, 0); //Máscara de Red
 
 int DBNum = 1; //Número de Datablock debe estar en el PLC (Configurado con Acceso Global)
-byte Buffer[20];//Buffer de datos, debe ser mayor a 10, maximo 1024 pero depende de la mamoria de Arduino
-byte BufferW[1];//Buffer para escritura de salidas
+//byte Buffer[1024];//Buffer de datos, debe ser mayor a 10, maximo 1024 pero depende de la mamoria de Arduino
+//byte BufferW[1];//Buffer para escritura de salidas
 
 S7Client Client; //Instancia de cliente S7
 
-unsigned long Elapsed; // To calc the execution time
+//unsigned long Elapsed; // To calc the execution time
 //----------------------------------------------------------------------
 // Setup : Init Ethernet and Serial port
 //----------------------------------------------------------------------
 void setup() {
     //Abrir puerto Serial
     Serial.begin(115200);
-    
-/*#ifdef S7WIFI
-//--------------------------------------------- ESP8266 Initialization    
-    Serial.println();
-    Serial.print("Connecting to ");
-    Serial.println(ssid);
-    WiFi.begin(ssid, pass);
-    WiFi.config(Local, Gateway, Subnet);
-    while (WiFi.status() != WL_CONNECTED) 
-    {
-        delay(500);
-        Serial.print(".");
-    }
-    Serial.println("");
-    Serial.println("WiFi connected");  
-    Serial.print("Local IP address : ");
-    Serial.println(WiFi.localIP());
-#else*/
 //--------------------------------Wired Ethernet Shield Initialization    
     // Start the Ethernet Library
     EthernetInit(mac, Local);
@@ -66,11 +43,10 @@ void setup() {
     Serial.println("");
     Serial.println("Conectado por cable...");  
     Serial.print("IP local");
-    Serial.println(Ethernet.localIP());
-//#endif   
+    Serial.println(Ethernet.localIP());  
 }
 //----------------------------------------------------------------------
-// Connects to the PLC
+// Conexión a PLC
 //----------------------------------------------------------------------
 bool Connect()
 {
@@ -87,7 +63,7 @@ bool Connect()
     return Result==0;
 }
 //----------------------------------------------------------------------
-// Dumps a buffer (a very rough routine)
+// Función para convertir BigEndian (S7) a littleEndian (Arduino)
 //----------------------------------------------------------------------
 void Dump(void *Buffer, int Length)//Funcion para convertir de BigEndian a LittleEndian
 {
@@ -117,7 +93,7 @@ void Dump(void *Buffer, int Length)//Funcion para convertir de BigEndian a Littl
   Serial.println("===============================================");
 }
 //----------------------------------------------------------------------
-// Prints the Error number
+// Funcoón para imprimir error de protoclo y tipo
 //----------------------------------------------------------------------
 void CheckError(int ErrNo)
 {
@@ -131,21 +107,7 @@ void CheckError(int ErrNo)
     Client.Disconnect(); 
   }
 }
-//----------------------------------------------------------------------
-// Profiling routines
-//----------------------------------------------------------------------
-/*void MarkTime()
-{
-  Elapsed=millis();
-}
-//----------------------------------------------------------------------
-void ShowTime()
-{
-  // Calcs the time
-  Elapsed=millis()-Elapsed;
-  Serial.print("Job time (ms) : ");
-  Serial.println(Elapsed);   
-}*/
+
 //----------------------------------------------------------------------
 // Main Loop
 //----------------------------------------------------------------------
@@ -153,7 +115,9 @@ void loop()
 {
   int Size, Result;
   void *Target;
-  void *TargetB;
+
+  //Size=1024;
+  //Target = &Buffer; // Uses a larger buffer
 
   Size=10;
   Target = NULL; // Uses the internal Buffer (PDU.DATA[])
@@ -166,25 +130,18 @@ void loop()
       delay(500);
   }
   
-  Serial.print("Reading ");Serial.print(Size);Serial.print(" bytes from DB");Serial.println(DBNum);
-  // Get the current tick
-  //MarkTime();
+  Serial.print("Leyendo ");Serial.print(Size);Serial.println(" bytes");
   //Areas probadas en el ejemplo: S7AreaPE: Entradas, S7AreaPA: Salidas, S7AreaMW: Merkers->Bytes de memoria MB
   Result=Client.ReadArea(S7AreaPE, //Area de lectura de entradas o DB, PE es para leer entradas, PA para leer salidas
-                         DBNum,    // Número de DB
+                         0,    // Número de DB a leer
                          0,        //Comenzar en byte 0
-                         Size,     //Leer 11 bytes
+                         Size,     //Leer 2 primeros bytes (DIa->byte 0, DIb->Byte1)
                          Target);  // Target es el buffer de almacenamiento de datos, si es NULL se almacenará internante, sino no se debe asignar la dirección del buffer da datos (ejm: Size=1024;Target = &Buffer;)
   if (Result==0)//Si resultado es 0 se recibierón los datos correctamente. Si el resultado mayor a 0 hubo un error
   {
-    //ShowTime();
     Dump(Target, Size); //Arreglar el formato de Big Endian a Little Endian para Arduino
   }else{
     CheckError(Result);
   }
-    
-  delay(500);  
-  BufferW[0]=0x01; //Byte para escribir en la salida, 
-  TargetB=&BufferW;
-  int Res=Client.WriteArea(S7AreaPA,0,0,1,TargetB); //Escribir en Área de salidas (Q0.0), etc. 
+  delay(500);
 }
