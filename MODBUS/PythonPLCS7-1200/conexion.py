92403f75600b58e6906b68bd745f8c7c62ebb7d9
Uniminutoarduino <digpot@gmail.com>
2022-04-28 21:58:39 -0500

    Create conexion.py

---
+++ MODBUS/PythonPLCS7-1200/conexion.py
@@ -0,0 +1,19 @@
+#Conexión MODBUS con PLC usando librería easymodbus
+#Creador: TalosElectrónico
+#Fecha: 28/04/2022
+import easymodbus.modbusClient #Importar libreria easymodbus como cliente
+import time
+
+
+if __name__=="__main__":
+    modbusclient=easymodbus.modbusClient.ModbusClient('192.168.0.20',502) #Conectar con PLC en IP y puerto 205
+    modbusclient.connect()
+
+    while True:
+        HoldingRegs = modbusclient.read_holdingregisters(0,2) #Leer dos registros desde la dirección 0
+        print(HoldingRegs)
+        discreteInputs = modbusclient.read_discreteinputs(0, 1) #Lectura de una entrada (I0.0)
+        print(discreteInputs) #Imprime verdadero o falso
+        modbusclient.write_single_register(2,1) #Escribe la posición 2 del array de registro de datos con el valor de 1
+        modbusclient.write_single_coil(1,1) #Activa la salida Q0.1 del PLCS7-1200
+        time.sleep(1)
