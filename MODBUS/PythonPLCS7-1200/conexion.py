106ff9888b3362ecc5b249cb835a4625c97f5642
Uniminutoarduino <digpot@gmail.com>
2022-04-28 22:02:42 -0500

    Update conexion.py

--- MODBUS/PythonPLCS7-1200/conexion.py
+++ MODBUS/PythonPLCS7-1200/conexion.py
@@ -11,9 +11,10 @@ if __name__=="__main__":
 
     while True:
         HoldingRegs = modbusclient.read_holdingregisters(0,2) #Leer dos registros desde la dirección 0
-        print(HoldingRegs)
+        print(HoldingRegs) #Imprime la información de los holding regs como una lista
+        print('Valor posición 0 de registro: '+str(HoldingRegs[0])) #inprimir posicion 0 del holding reg
         discreteInputs = modbusclient.read_discreteinputs(0, 1) #Lectura de una entrada (I0.0)
-        print(discreteInputs) #Imprime verdadero o falso
+        print('Valor lectura entrada I0.0: '+str(discreteInputs)) #Imprime verdadero o falso de acuerdo al valor lógico de la entrada leida
         modbusclient.write_single_register(2,1) #Escribe la posición 2 del array de registro de datos con el valor de 1
         modbusclient.write_single_coil(1,1) #Activa la salida Q0.1 del PLCS7-1200
         time.sleep(1)
